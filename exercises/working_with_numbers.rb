935ca073a4cdae3d4461419510c4dbf8e665a48b
Garrett Cooper <garrett.l.cooper@gmail.com>
2017-10-29 00:07:26 -0400

    exercise 1.4

--- exercises/working_with_numbers.rb
+++ exercises/working_with_numbers.rb
@@ -4,7 +4,7 @@ require 'date'
 def format_number(number)
   number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
 end
-# Exercises 1.1
+# Exercise 1.1
 # How many hours are in a year.
 # 1 year = 365 days / year * 24 hours / day
 puts "There are #{format_number(365 * 24)} hours in one year."
@@ -17,3 +17,9 @@ puts "There are #{format_number(10* 365 * 24 * 60)} minutes in one decade."
 birth_time = DateTime.new(1979,6,24,23,58).to_time.to_i
 now_time = Time.now.to_i
 puts "I am #{format_number(now_time - birth_time)} seconds old."
+
+# Exercise 1.4
+# Use even? or odd? method to determine if a certain number is odd or even
+(1..10).each do |n|
+  puts n.even? ? "#{n} is even" : "#{n} is odd"
+end
